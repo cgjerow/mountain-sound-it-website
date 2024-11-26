427f88f13d2ace9c6efc1d5ec2cd45607dd0b21e
connorjerow <cgjerow@gmail.com>
2024-11-26 10:42:00 -0800

    initial site

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 export default (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy("src/hero.svg");
 
   return {
     // When a passthrough file is modified, rebuild the pages:
