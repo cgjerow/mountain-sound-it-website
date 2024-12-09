c4429d0ae0aaa1702d220aace825e32559fb973e
connorjerow <cgjerow@gmail.com>
2024-12-09 09:46:47 -0800

    add project config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   outDir: "docs",
+  site: "https://cgjerow.github.io",
+  base: "mountain-sound-it-website",
 });
